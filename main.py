102d205790e1f07273dfcea246a36f063df5e587
Nassim Fares <nassim.fares.dz@viacesi.fr>
2020-05-04 05:39:11 +0100

    enemy instantiation

--- main.py
+++ main.py
@@ -20,6 +20,18 @@ magic = [
 ]
 
 player = Person(460, 65, 60, 34, magic)
+enemy = Person(1200, 65, 45, 25, magic)
 
-print(player.generate_spell_damage(0))
-print(player.generate_spell_damage(1))
+run = True
+print(BColors.FAIL + BColors.BOLD + "AN ATTACKS !!!!" + BColors.ENDC)
+
+while run:
+    print("=============================")
+    player.choose_action()
+    index = int(input("Choose action : ")) - 1
+    if index == 0:
+        dmg = player.generate_damage()
+        enemy.take_damage(dmg)
+        print("you attack for ", dmg, " points of damage .\n Enemy HP is : ", enemy.hp)
+
+
